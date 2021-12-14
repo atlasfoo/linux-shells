71ebfe3489cca56cd59b1b32a6f7aad808f5c5eb
atlasfoo@pop <jf_mejiar547@live.com>
2021-12-13 23:43:40 -0600

    AWS CLI installer (global linux)

---
+++ deb-ubuntu/aws_sdk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing AWS CLI from official site"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+echo "AWS CLI successfully installed"
