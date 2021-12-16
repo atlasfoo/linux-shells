12b47760da4c68fdbba7e35bf26d0e9ff87bfe25
atlasfoo@pop <jf_mejiar547@live.com>
2021-12-15 22:48:26 -0600

    updated nvm root shell to comply with sudo source command limitations@

--- deb-ubuntu/nvm_root_install.sh
+++ deb-ubuntu/nvm_root_install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 echo "Installing most recent nvm from github repository"
 
-sudo git clone https://github.com/creationix/nvm.git /opt/nvm
+sudo -s
 
-sudo mkdir /usr/local/nvm
+git clone https://github.com/creationix/nvm.git /opt/nvm
 
-sudo echo "export NVM_DIR=/usr/local/nvm
+mkdir /usr/local/nvm
+
+echo "export NVM_DIR=/usr/local/nvm
 source /opt/nvm/nvm.sh" | sudo tee ~/.bashrc -a
 
-sudo source ~/.bashrc
+source ~/.bashrc
 
-sudo echo '#!/bin/bash
+echo '#!/bin/bash
 VERSION=`cat /usr/local/nvm/alias/default`
 export PATH="/usr/local/nvm/versions/node/v$VERSION/bin:$PATH"' | sudo tee /etc/profile.d/nvm.sh
 
-sudo chmod +x /etc/profile.d/nvm.sh
+chmod +x /etc/profile.d/nvm.sh
 
 read -p "nvm successfully installed as root"
